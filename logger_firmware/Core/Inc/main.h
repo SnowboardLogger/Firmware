039a2b12ffaabffc4c9048a65e7e3ae403ac8d67
ajguido <ajguido@eecs373-06.adsroot.itcs.umich.edu>
2023-04-10 18:11:55 -0400

    Initial push

---
+++ logger_firmware/Core/Inc/main.h
@@ -0,0 +1,118 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+typedef enum {
+	speed,
+	alt,
+	longest,
+	tallest,
+	startLog,
+	stopLog,
+	pauseLog,
+	resumeLog,
+	save,
+	error
+} screenStates;
+volatile extern screenStates state;
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MCO_Pin GPIO_PIN_0
+#define MCO_GPIO_Port GPIOA
+#define SD_SPI_SCK_Pin GPIO_PIN_1
+#define SD_SPI_SCK_GPIO_Port GPIOA
+#define BT_UART_TX_Pin GPIO_PIN_2
+#define BT_UART_TX_GPIO_Port GPIOA
+#define BT_UART_RX_Pin GPIO_PIN_3
+#define BT_UART_RX_GPIO_Port GPIOA
+#define SD_SPI_MISO_Pin GPIO_PIN_6
+#define SD_SPI_MISO_GPIO_Port GPIOA
+#define SD_SPI_MOSI_Pin GPIO_PIN_7
+#define SD_SPI_MOSI_GPIO_Port GPIOA
+#define QiEnable_GPIO_Pin GPIO_PIN_1
+#define QiEnable_GPIO_GPIO_Port GPIOB
+#define GPS_UART_TX_Pin GPIO_PIN_9
+#define GPS_UART_TX_GPIO_Port GPIOA
+#define GPS_UART_RX_Pin GPIO_PIN_10
+#define GPS_UART_RX_GPIO_Port GPIOA
+#define Button_GPIO_Pin GPIO_PIN_11
+#define Button_GPIO_GPIO_Port GPIOA
+#define Button_GPIO_EXTI_IRQn EXTI15_10_IRQn
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define LED_GPIO_Pin GPIO_PIN_3
+#define LED_GPIO_GPIO_Port GPIOB
+#define Button_GPIOB4_Pin GPIO_PIN_4
+#define Button_GPIOB4_GPIO_Port GPIOB
+#define Button_GPIOB4_EXTI_IRQn EXTI4_IRQn
+#define Button_GPIOB5_Pin GPIO_PIN_5
+#define Button_GPIOB5_GPIO_Port GPIOB
+#define Button_GPIOB5_EXTI_IRQn EXTI9_5_IRQn
+#define Display_I2C_SCL_Pin GPIO_PIN_6
+#define Display_I2C_SCL_GPIO_Port GPIOB
+#define Display_I2C_SDA_Pin GPIO_PIN_7
+#define Display_I2C_SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ logger_firmware/Core/Src/stm32l4xx_it.c
@@ -0,0 +1,357 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32l4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "liquidcrystal_i2c.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+volatile extern screenStates state;
+volatile extern screenStates prevState;
+volatile extern uint8_t isLogging;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+  while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Prefetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32L4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32l4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line4 interrupt.
+  */
+void EXTI4_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI4_IRQn 0 */
+
+	// NOTE: ALL INTERRUPTS SUBJECT TO CHANGE/REORDERING
+	switch(state) {
+			case speed: {
+				prevState = state;
+				state = alt;
+				break; }
+			case alt: {
+				prevState = state;
+				state = longest;
+				break; }
+			case longest: {
+				prevState = state;
+				state = tallest;
+				break; }
+			case tallest: {
+				prevState = state;
+				state = speed;
+				break; }
+			case startLog: {
+				state = prevState;
+				break; }
+			case stopLog: {
+				state = prevState;
+				break; }
+			case pauseLog: {
+				state = prevState;
+				break; }
+			case resumeLog: {
+				break; }
+			case save: {
+				state = prevState;
+				break; }
+			default: {
+				prevState = state;
+				state = error;
+				break; }
+		}
+
+  /* USER CODE END EXTI4_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(Button_GPIOB4_Pin);
+  /* USER CODE BEGIN EXTI4_IRQn 1 */
+
+  /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+	// NOTE: ALL INTERRUPTS SUBJECT TO CHANGE/REORDERING
+	switch(state) {
+		case speed: {
+			prevState = state;
+			state = isLogging ? stopLog : startLog;
+			break; }
+		case alt: {
+			prevState = state;
+			state = isLogging ? stopLog : startLog;
+			break; }
+		case longest: {
+			prevState = state;
+			state = isLogging ? stopLog : startLog;
+			break; }
+		case tallest: {
+			prevState = state;
+			state = isLogging ? stopLog : startLog;
+			break; }
+		case startLog: {
+			isLogging = 1;
+			state = prevState;
+			break; }
+		case stopLog: {
+			isLogging = 0;
+			state = prevState;
+			break; }
+		case pauseLog: {
+			// stop log
+			break; }
+		case resumeLog: {
+			break; }
+		case save: {
+
+			break; }
+		default: {
+
+			break; }
+	}
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(Button_GPIOB5_Pin);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+	// NOTE: ALL INTERRUPTS SUBJECT TO CHANGE/REORDERING
+	switch(state) {
+		case speed: {
+			state = isLogging ? pauseLog : state;
+			break; }
+		case alt: {
+			state = isLogging ? pauseLog : state;
+			break; }
+		case longest: {
+			state = isLogging ? pauseLog : state;
+			break; }
+		case tallest: {
+			state = isLogging ? pauseLog : state;
+			break; }
+		case startLog: {
+			state = pauseLog;
+			break; }
+		case stopLog: {
+			break; }
+		case pauseLog: {
+			state = resumeLog;
+			break; }
+		case resumeLog: {
+			state = prevState;
+			break; }
+		case save: {
+			break; }
+		default: {
+
+			break; }
+	}
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(Button_GPIO_Pin);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
